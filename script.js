df3ff58092609ad35eb8e70f26b6bcb169abd674
khalidjambi <khalid.jambi18@gmail.com>
2019-04-30 14:12:44 +0700

    first

---
+++ script.js
@@ -0,0 +1,26 @@
+// alert('Helo apa kabar');
+// alert('nama');
+// alert('saya');
+// alert('Khalid ibrahim');
+
+// var nama = prompt('masukan nama:');
+// alert(nama);
+
+// var tes = confirm('Kamu yakin?');
+// if ( tes == true ){
+//     alert('user menekan OK!');
+// } else {
+//     alert('user menekan CANCEL!');
+// }
+
+alert('Halo selamat datang');
+var lagi = true;
+
+while(lagi) {
+    var nama = prompt('masukan nama:');
+    alert('halo ' + nama);
+
+    lagi = confirm('coba lagi?');
+}
+
+alert('terimakasih');
